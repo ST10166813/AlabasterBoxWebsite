76a43f409aa7b494d62c70db7f8e3a60a33dbe28
ST10050455 <127688706+ST10050455@users.noreply.github.com>
2025-10-27 19:43:06 +0200

    updatedTextFieldLayout

--- Alabaster/obj/Debug/net9.0/Alabaster.AssemblyInfo.cs
+++ Alabaster/obj/Debug/net9.0/Alabaster.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9012cfdffbd4b8bd0e9818078de5a229d005c4bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678e660afe4cac006cc6f2f1db75e5eb7fcb4bcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
