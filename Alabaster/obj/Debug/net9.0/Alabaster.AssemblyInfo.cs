7f457f4d216e055cdaf2a2aea9ee5975dffd04f8
Niska Pillay <130572748+ST10166813@users.noreply.github.com>
2025-10-27 18:19:55 +0200

    Updated the donation screen

--- Alabaster/obj/Debug/net9.0/Alabaster.AssemblyInfo.cs
+++ Alabaster/obj/Debug/net9.0/Alabaster.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65725456505d4e9ebe8cc5f8189e890e61476344")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc657a239449d0e948fbd2ad3246bea7a508a86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
