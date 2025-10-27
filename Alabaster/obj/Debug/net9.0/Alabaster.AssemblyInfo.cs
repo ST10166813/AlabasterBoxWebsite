65725456505d4e9ebe8cc5f8189e890e61476344
kamil maharaj <ST10068456@vcconnect.edu.za>
2025-10-27 13:56:43 +0200

    updated code

--- Alabaster/obj/Debug/net9.0/Alabaster.AssemblyInfo.cs
+++ Alabaster/obj/Debug/net9.0/Alabaster.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678e660afe4cac006cc6f2f1db75e5eb7fcb4bcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd21f0383b0ec3c6a9df434f4ba0ff04a73abdac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
