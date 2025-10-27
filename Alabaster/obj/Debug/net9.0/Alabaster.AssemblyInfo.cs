50ecf00e7b2c994562d6c810868e3297da9c4418
ST10050455 <127688706+ST10050455@users.noreply.github.com>
2025-10-27 10:42:52 +0200

    interfaceUpdated

--- Alabaster/obj/Debug/net9.0/Alabaster.AssemblyInfo.cs
+++ Alabaster/obj/Debug/net9.0/Alabaster.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a02d4a3f778dbd8f384614c5c6fc124a1758c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9012cfdffbd4b8bd0e9818078de5a229d005c4bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
