39e3df257011995e7d524d6dea0c100c3b5f2031
ST10050455 <127688706+ST10050455@users.noreply.github.com>
2025-11-04 16:36:00 +0200

    renamed everything again

--- Alabaster/obj/Debug/net9.0/Alabaster.AssemblyInfo.cs
+++ Alabaster/obj/Debug/net9.0/Alabaster.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a2939a361af60441db5a1a9946790b24f825bcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21cb33312e5e5d5ea538cbbc299dab8a004edd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alabaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
